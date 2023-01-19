7d6a461458532754bea52cd40366d59e9ba00a10
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-01-19 20:15:23 -0300

    Add files via upload

---
+++ AprendendoFiles.cs
@@ -0,0 +1,49 @@
+// Criando e escrevendo em um arquivo de texto
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AprendendoFiles
+{
+	
+	class Program
+	{
+	static void Main(string[] args)
+		{
+			StreamWriter escritor = new StreamWriter("teste.txt");
+			escritor.WriteLine("Alison");
+			escritor.WriteLine("Moreira");
+			escritor.WriteLine("Curso de C#");
+			escritor.Close();
+			Console.WriteLine("Arquivo gerado");
+			Console.ReadLine();
+		}
+	}
+}
+
+// Adicionando novas informações um arquivo de texto
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AprendendoFiles
+{
+	
+	class Program
+	{
+	static void Main(string[] args)
+		{
+			/*É necessário mudar apenas uma linha do programa, no qual ao invés de receber `StreamWriter` a recebera o parametro de `File.AppendText`*/
+			StreamWriter escritor = File.AppendText("teste.txt");
+			escritor.WriteLine("Alison");
+			escritor.WriteLine("Moreira");
+			escritor.Close();
+			Console.WriteLine("Arquivo gerado");
+			Console.ReadLine();
+		}
+	}
+}
