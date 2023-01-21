7e65fcf45d30a428e2b454f9c0193c2a5a4e426a
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-01-20 23:48:59 -0300

    Update AprendendoFiles.cs

--- AprendendoFiles.cs
+++ AprendendoFiles.cs
@@ -47,3 +47,31 @@ namespace AprendendoFiles
 		}
 	}
 }
+
+// Lendo um arquivo de texto com estruturas de repetição
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AprendendoFiles
+{
+	
+	class Program
+	{
+	static void Main(string[] args)
+		{
+			StreamReader leitor = new StreamReader("teste.txt");
+		
+			string = "";
+			while(linha != null)
+			{
+				linha = leitor.ReadLine();
+				if(linha != null)
+			}
+			Console.WriteLine(linha);
+		}
+	}
+	leitor.Close();
+}
